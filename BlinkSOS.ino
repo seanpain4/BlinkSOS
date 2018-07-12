9fce54649945910fb2f95ae94029e5eb3ba56499
Sean Pain <seanpain4@gmail.com>
2018-07-12 22:31:04 +1000

    Final

--- BlinkSOS.ino
+++ BlinkSOS.ino
@@ -1,12 +1,10 @@
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // Initialize LED_BUILTIN as output
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+//Fucntion to blink letter 'o'
 void morse_o() {
-  //Fucntion to blink letter 'o'
   digitalWrite(LED_BUILTIN, HIGH);
   delay(450);
   digitalWrite(LED_BUILTIN, LOW);
@@ -20,9 +18,8 @@ void morse_o() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-
+//Fucntion to blink letter 's'
 void morse_s() {
-  //Fucntion to blink letter 's'
   digitalWrite(LED_BUILTIN, HIGH);
   delay(150);
   digitalWrite(LED_BUILTIN, LOW);
@@ -36,9 +33,7 @@ void morse_s() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-
-
-// the loop function runs over and over again forever
+// Loops 'SOS' in morse code forever
 void loop() {
   morse_s();
   delay(450);
